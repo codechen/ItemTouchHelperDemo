63199d38809658eefa28a6c73f45462aed63fa61
chenchao <chenchao@evergrande.com>
2019-06-13 16:53:47 +0800

    init'

---
+++ app/src/main/java/co/paulburke/android/itemtouchhelperdemo/RecyclerListAdapter.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2015 Paul Burke
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.paulburke.android.itemtouchhelperdemo;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import co.paulburke.android.itemtouchhelperdemo.helper.ItemTouchHelperAdapter;
+import co.paulburke.android.itemtouchhelperdemo.helper.ItemTouchHelperViewHolder;
+
+/**
+ * Simple RecyclerView.Adapter that implements {@link ItemTouchHelperAdapter} to respond to move and
+ * dismiss events from a {@link android.support.v7.widget.helper.ItemTouchHelper}.
+ *
+ * @author Paul Burke (ipaulpro)
+ */
+public class RecyclerListAdapter extends RecyclerView.Adapter<RecyclerListAdapter.ItemViewHolder>
+        implements ItemTouchHelperAdapter {
+
+    private final List<String> mItems = new ArrayList<>();
+    private final List<String> mColors = new ArrayList<>();
+
+    public RecyclerListAdapter(Context context) {
+        mItems.addAll(Arrays.asList(context.getResources().getStringArray(R.array.dummy_items)));
+        mColors.addAll(Arrays.asList(context.getResources().getStringArray(R.array.item_color)));
+    }
+
+    @Override
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.pager_main, parent, false);
+        ItemViewHolder itemViewHolder = new ItemViewHolder(view);
+        return itemViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final ItemViewHolder holder, int position) {
+        holder.textView.setText(mItems.get(position));
+        holder.handleView.setBackgroundColor(Color.parseColor(String.valueOf(mColors.get(position))));
+    }
+
+    @Override
+    public void onItemDismiss(int position) {
+        mItems.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    /**
+     * Simple example of a view holder that implements {@link ItemTouchHelperViewHolder} and has a
+     * "handle" view that initiates a drag event when touched.
+     */
+    public static class ItemViewHolder extends RecyclerView.ViewHolder implements
+            ItemTouchHelperViewHolder {
+
+        public final TextView textView;
+        public final LinearLayout handleView;
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.text);
+            handleView = (LinearLayout) itemView.findViewById(R.id.item);
+        }
+
+        @Override
+        public void onItemSelected() {
+//            itemView.setBackgroundColor(Color.parseColor("#DB7093"));
+        }
+
+        @Override
+        public void onItemClear() {
+//            itemView.setBackgroundColor(Color.parseColor("#DB7093"));
+        }
+    }
+}
